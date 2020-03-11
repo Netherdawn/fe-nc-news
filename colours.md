dfb35abcf4fffe27bfe5cf3664bb17cecbd1c6ac
Brork <jamessamueljohnson@gmail.com>
2020-03-11 22:24:25 +0000

    testing header set up

---
+++ colours.md
@@ -0,0 +1,13 @@
+White to dark
+
+#fffcd9 - white
+#f2f6f8 - pale white
+#b5d2e1 - blue grey
+#cddbe2 - silver grey
+#7695a5 - grey
+#394f5a - dark grey
+
+Reds
+#e92e4b - maroon
+#d0021b - red
+#f57403 - orange
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <Span>
+        ><h1 id="red-n">N</h1>
+        <h1>C News</h1>
+      </Span>
+    </div>
+  );
+}
+
+export default Header;
